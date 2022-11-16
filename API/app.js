62d20309cc20b3dd0aa5c9be59bd1e8bb9b50580
Javier <17171467@itculiacan.edu.mx>
2022-11-16 15:10:18 -0700

    API node js

---
+++ API/app.js
@@ -0,0 +1,8 @@
+let express = require('express');
+let mysql = require('mysql');
+let app = express();
+let puerto = 3000;
+
+app.listen(puerto, function(){
+    console.log("Servidor En Linea");
+});
